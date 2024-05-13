524c0d2d1e158745dc71a97ea1f50ae6a5c3690a
Griffin Moyer <gmoyer@goldensoftware.com>
2024-05-13 15:05:05 -0600

    Updated readme and example script

--- Deployment/example-script.py
+++ Deployment/example-script.py
@@ -1,5 +1,6 @@
 from identifysnowpack import identify_image
 
 input_path = input("Enter the input image: ")
+shape_path = input("Enter the shapefile: ")
 output_path = input("Enter the output image: ")
-identify_image(input_path, output_path)
+identify_image(input_path, shape_path, output_path)
